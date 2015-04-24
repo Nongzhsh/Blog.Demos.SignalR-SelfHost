30312a2c0da58bed973ecb936243d6226ca100ba
Richard Niemand <niemand.richard@gmail.com>
2015-04-24 18:23:42 +0200

    Initial Commit

---
+++ SelfHostedSignalR/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace SelfHostedSignalR
+{
+    // Adapted from :: http://www.asp.net/signalr/overview/deployment/tutorial-signalr-self-host#server
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string url = "http://localhost:8080";
+
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+}
